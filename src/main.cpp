35e4720afd9658f1d7f9f417282df0ca0130ed83
Johan Kjölhede <kjolhede@telia.com>
2015-02-01 10:38:36 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -18,36 +18,31 @@ int main() {
 
 	const std::vector<int> src = { 1, 2, 3 };
 
+	// Free function use
 	auto squares = map(src, [](int x) {return x*x;});
 	auto largerThan1 = filter(src, [](int x) {return x > 1;});
 	auto strings = map(src, [](int x) {return std::to_string(x);});
-
-	Monadic<std::vector<int>> m(src);
-
-	auto mMapped = m.map([](int x) {return x*x;});
-	auto mFiltered = m.filter([](int x) {return x > 1;});
-
-	printAll(src, "src");
-	printAll(largerThan1, "largerThan1");
-	printAll(strings, "strings");
-	printAll(squares, "squares");
-
-	printAll(mMapped.col, "mMapped");
-
-	std::vector<std::vector<int>> intLists;
-	intLists.push_back(src);
-
+	std::vector<std::vector<int>> intLists{src};
 	std::vector<std::vector<int>> mapped = map(src, [](int x) { return std::vector<int>{1,2,3};} );
 	std::vector<int> flatMapped = flatMap(src, [](int x) { return std::vector<int>{1,2,3};} );
 	std::vector<int> flattened = flatten(mapped);
 
+	// Monadic use
+	Monadic<std::vector<int>> m(src);
+	auto mMapped = m.map([](int x) {return x*x; });
+	auto mFiltered = m.filter([](int x) {return x > 1; });
 	std::vector<int> funList = 
 		monadic(src)
 			.map([](int x) { return std::vector<int>{1,2,3};} )
 			.flatten()
 			.filter([](int x) { return x > 1; })
 			.col;
-
+	
+	printAll(src, "src");
+	printAll(largerThan1, "largerThan1");
+	printAll(strings, "strings");
+	printAll(squares, "squares");
+	printAll(mMapped.col, "mMapped");
 	printAll(funList, "funList");
 
 	return 0;
